225d69fdfbe1515059be4dd3d1be08d4976f8d7c
SimranShaikh-28 <simrana.s2896@gmail.com>
2025-09-07 12:56:13 +0530

    Update admin_login.py
    add button

--- admin_login.py
+++ admin_login.py
@@ -10,7 +10,7 @@ print('''
     <meta charset="utf-8" />
     <title>Admin Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="assets/images/image.png">
     <link href="assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
     <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
